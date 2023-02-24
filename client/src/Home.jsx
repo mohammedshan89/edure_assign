57a07be95dfa7d45e544f6da74c50d5ec7ca4605
Moahmmed Shan S <mohammedshanshajahan@gmail.com>
2023-02-24 18:25:29 +0530

    api call finished

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,11 +1,25 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import MovieDetails from "./Components/MovieDetails";
 import MovieForm from "./Components/MovieForm"
 
 const Home = () => {
+  const [movies, setMovies] = useState([])
+  useEffect(()=>{
+    const fetchMovies = async() => {
+      const movies = await axios.get("http://localhost:4040/api/movies")
+      setMovies(movies.data)
+    }
+
+    fetchMovies()
+  },[])
   return (
     <div className="home">
       <div className="workout">
-        <MovieDetails />
+      {movies.map((movie)=>(
+        
+        <MovieDetails key={movie._id} movie = {movie} />
+      ))}
       </div>
       <MovieForm />
     </div>
