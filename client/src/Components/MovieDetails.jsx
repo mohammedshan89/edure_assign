29f7a2012b616e8b04ea86f96cd3e0ecd5344123
Moahmmed Shan S <mohammedshanshajahan@gmail.com>
2023-02-24 17:46:44 +0530

    user interface finished

---
+++ client/src/Components/MovieDetails.jsx
@@ -0,0 +1,31 @@
+
+import React from "react";
+import { BsFillTrashFill } from "react-icons/bs";
+
+
+const MovieDetails = () => {
+  
+  
+
+  return (
+    <div className="workout-details">
+      <h4>name</h4>
+      <p>
+        <strong>Load (kg) : </strong>
+        dsg
+      </p>
+      <p>
+        <strong>Reps :</strong>
+        gfgfds
+      </p>
+     
+
+      <span onClick={''}>
+         <BsFillTrashFill color="red" />
+      </span>
+    </div>
+  );
+};
+
+export default MovieDetails;
+
